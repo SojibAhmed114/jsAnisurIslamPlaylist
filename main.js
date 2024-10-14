e055b9a6d63389780f454f27b71ac0ff35fdd09b
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-10-14 06:15:15 +0600

    missing file upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// alert("please like my facebook page and youtube channel");
+
+// document.write("name: sojib ahmed safi <br>");
+// document.write("mobile number: 014545564");
+
+
+//  number to string convert 
+let myNumber = 500;
+console.log(typeof (myNumber));
+myNumber = toString(myNumber);
+
+
+//  String to number convert 
+let fatherName = "Abdus salam";
+fatherName = Number(fatherName);
+console.log(typeof (fatherName));
+
+let myMoney = "23";
+myMoney = parseInt(myMoney);
+console.log(typeof(myMoney));
+
+let myMoneyTwo = "29.6345";
+myMoneyTwo = parseFloat(myMoneyTwo);
+console.log(typeof(myMoneyTwo));
+
+
+let tk = 254.12458;
+let tkCon = tk.toFixed(2)
+console.log(typeof(tkCon));
+
+//terminal setup
+var x = 140;
+let y = 120;
+console.log(x+y);
